1b08188311d1bcece4c9a7e2111c7546dceebc61
sgsayani <sgsayanighatak@gmail.com>
2025-08-28 00:57:45 +0530

    description-jsx-css

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Elegant Floral Summer Dress – Crafted from lightweight fabric for all-day comfort. Perfect for casual outings, parties, or daily wear. Available in multiple sizes and colors.</p>
+        <p>Discover the latest trends in fashion, electronics, and more—all in one place. Shop with ease, enjoy secure payments, and get fast delivery right to your doorstep.Your style, your shop! From chic outfits to must-have gadgets, explore thousands of products handpicked just for you. Shop smart, shop easy!</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
