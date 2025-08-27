8d50a9044942cf065ece5bbf20c94fa2ec713dd5
sgsayani <sgsayanighatak@gmail.com>
2025-08-28 00:25:29 +0530

    CSS for productdisplay

--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -3,8 +3,9 @@ import './Breadcrum.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png';
 import all_product from '../Assets/all_product';  // <-- import array
 
-const Breadcrum = ({ productId }) => {
-  const product = all_product.find((item) => item.id === productId);
+const Breadcrum = (props) => {
+  const { product } = props;
+  // const product = all_product.find((item) => item.id === productId);
   if (!product) {
     return null;
   }
