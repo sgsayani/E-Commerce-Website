106702ceec12fb0fadfcf45a37b5c8f51500c000
sgsayani <sgsayanighatak@gmail.com>
2025-08-31 00:53:18 +0530

    complete frontend part for desktop view

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,7 @@ import { ShopContext } from '../../Context/ShopContext';
 import remove_icon from '../Assets/cart_cross_icon.png';
 
 const CartItems = () => {
-  const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
+  const {getTotalCartAmount, all_product, cartItems, removeFromCart } = useContext(ShopContext);
 
   return (
     <div className='cartitems'>
@@ -45,7 +45,7 @@ const CartItems = () => {
             <div>
                 <div className="cartitems-total-item">
                     <p>Subtotal</p>
-                    <p>${0}</p>
+                    <p>${getTotalCartAmount()}</p>
                 </div>
                 <hr />
                 <div className="cartitems-total-item">
@@ -55,7 +55,7 @@ const CartItems = () => {
                 <hr />
                 <div className="cartitems-total-item">
                     <h3>Total</h3>
-                    <h3>${0}</h3>
+                    <h3>${getTotalCartAmount()}</h3>
                 </div>
             </div>
             <button>Proceed to Checkout</button>
