774812a80ae12b87dcef718b880c8e10ba1a0f31
sgsayani <sgsayanighatak@gmail.com>
2025-08-30 11:32:23 +0530

    add related producs

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './RelatedProducts.css'
 import data_product from '../Assets/data'
-import Item from './Item'
+import Item from '../Item/Item'                
 
 const RelatedProducts = () => {
   return (
     <div className='relatedproducts'>
       <h1>Related Products</h1>
       <hr />
-      <div className="relatedproducs-item">
+      <div className="relatedproducts-item">
         {data_product.map((item,i)=>{
           return<Item 
           key={i} 
