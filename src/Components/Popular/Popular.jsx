1c281814e92cb4eb2d1869dfcbba0e05ef50976d
sgsayani <sgsayanighatak@gmail.com>
2025-09-07 02:36:15 +0530

    implement api in fe

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import data_product from '../Assets/data'   
+import React,{useState,useEffect} from 'react'
+// import data_product from '../Assets/data'   
 import Item from '../Item/Item'                
 import './Popular.css'
 const Popular = () => {
+  const [popular_data_product,setPopularData_product]=useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/popularcollections")
+      .then((response)=> response.json())
+      .then((data)=>{setPopularData_product(data)});
+  },[])
+
   return (
     <div className='popular'>
       <h1>POPULAR IN WOMEN</h1>
       <hr />
       <div className='popular-item'>
-        {data_product.map((item, i) => (
+        {popular_data_product.map((item, i) => (
           <Item 
             key={i} 
             id={item.id} 
